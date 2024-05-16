2cd991bc92e0be2db92a4cddb60fce1403086510
paulalextech <psteane123@hotmail.com>
2024-05-16 11:42:08 +0100

    initial input section

--- name.py
+++ name.py
@@ -1,8 +1,16 @@
 print('Hello world')
 
 # Option for user input
+# what country do you like food from?
+
+input("What food do you like?")
+
+print("food country")
+
+
 
 # Returns list of options
+
 
 # Interacts with Dataset
 
